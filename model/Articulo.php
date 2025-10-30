225d1adc337e3bd3ac1b9b324e2d2c4208ae7d58
Rayco-jpg <gutierrezrayco13@gmail.com>
2025-10-30 11:52:44 +0100

    commit de la clase Articulo

---
+++ model/Articulo.php
@@ -0,0 +1,49 @@
+<?php
+// Clase Artículo
+class articulo{
+    private $nombre;
+    private $precio;
+    private $disponibilidad;
+    private $categoria;
+
+    public function __construct($nombre, $precio, $disponibilidad, $categoria) {
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->disponibilidad = $disponibilidad;
+        $this->categoria = $categoria;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getPrecio() {
+        return $this->precio;
+    }
+
+    public function getDisponible() {
+        return $this->disponibilidad;
+    }
+
+    public function getCategoria() {
+        return $this->categoria;
+    }
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function setPrecio($precio) {
+        $this->precio = $precio;
+    }
+
+    public function setDisponible($disponibilidad) {
+        $this->disponibilidad = $disponibilidad;
+    }
+
+    public function setCategoria($categoria) {
+        $this->categoria = $categoria;
+    }
+
+}
+?>
